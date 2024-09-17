c72eeaa9cafa446562cb7657e32fa52d01b1def6
Dtnexo <pt22ugm@eduvaud.ch>
2024-09-17 17:22:55 +0200

    feat(Drone): Afficher les bâtiments

--- DiegoTeixeiraNunes/Drones/Drones/Program.cs
+++ DiegoTeixeiraNunes/Drones/Drones/Program.cs
@@ -14,11 +14,14 @@ namespace Drones
 
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
+            List<building> batiment = new List<building>();
             Drone drone = new Drone("Joe", 100, 100);
+            building bat = new building(34, 54, 100, 200, "red");
             fleet.Add(drone);
+            batiment.Add(bat);
 
             // Démarrage
-            Application.Run(new AirSpace(fleet));
+            Application.Run(new AirSpace(fleet, batiment));
         }
     }
 }
---
+++ DiegoTeixeiraNunes/Drones/Drones/building.cs
@@ -0,0 +1,31 @@
+﻿using Drones.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    class building
+    {
+        protected int _x,_y;
+        protected int _largeur;
+        protected int _profondeur;
+        protected string _couleur;
+        private Pen build = new Pen(new SolidBrush(Color.Purple), 3);
+        public building(int posx, int posy, int width , int height, string color) 
+        { 
+            _x = posx;
+            _couleur = color;
+            _y = posy;
+            _largeur = width;
+            _profondeur = height;
+        }
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            drawingSpace.Graphics.DrawEllipse(build, new Rectangle(_x - 4, _y - 2, 8, 8));
+            drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, _x + 5, _y - 5);
+        }
+    }
+}
