92d4a5a5041afbf57c8e2a3c75daed4c90df9397
Dtnexo <pt22ugm@eduvaud.ch>
2024-10-01 17:20:02 +0200

    feat(Drone): Interface non fini

--- DiegoTeixeiraNunes/Drones/Drones/Model/Drone.cs
+++ DiegoTeixeiraNunes/Drones/Drones/Model/Drone.cs
@@ -1,9 +1,31 @@
 ﻿using Drones.Helpers;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Drones
 {
+    
+    public enum EvacuationState
+    {
+        Evacuating,     // Limits known, moving out of the zone
+        Free,           // No limits applied
+        Evacuated       // Limits known, out of the zone
+    }
+
+    public interface IExpellable
+    {
+        // Signal the limits of the no-fly zone 
+        // Return true if the drone is already outside the zone
+        public bool Evacuate(Rectangle zone);
+
+        // Terminate the no-fly zone
+        public void FreeFlight();
+
+        // Interrogate the drone
+        public EvacuationState GetEvacuationState();
+    }
     // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
-    public partial class Drone
+    public partial class Drone : IExpellable
     {
         Random alea = new Random();
 
@@ -11,8 +33,8 @@ namespace Drones
         private string _name;                           // Un nom
         private int _x ;                                // Position en X depuis la gauche de l'espace aérien
         private int _y;                                 // Position en Y depuis le haut de l'espace aérien
-
-
+        public bool zone;
+        private Pen NoflyBrush = new Pen(new SolidBrush(Color.Black), 3);
         // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
         // que 'interval' millisecondes se sont écoulées
         public Drone (string nom, int corx, int cory)
@@ -29,5 +51,45 @@ namespace Drones
             charge--;                                  // Il a dépensé de l'énergie
         }
 
+        public bool Evacuate(Rectangle zone)
+        {
+            if (_x > zone.X && _x < zone.X+zone.Width && _y > zone.Y && _y < zone.Y+zone.Width)
+            {
+                while (_x > zone.X && _x < zone.X + zone.Width)
+                {
+                    _x--;
+                }
+                while (_y > zone.Y && _y < zone.Y + zone.Width)
+                {
+                    _y--;
+                }
+                return true;
+            }
+            return false;
+            void Render(BufferedGraphics drawingSpace)
+            {
+                drawingSpace.Graphics.DrawEllipse(NoflyBrush, new Rectangle(_x - 4, _y - 2, 8, 8));
+                drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, _x + 5, _y - 5);
+            }
+        }
+        public void FreeFlight() 
+        {
+            if (_x < 400)
+            {
+                zone = true;
+            }
+            zone = false;   
+
+        }
+        public EvacuationState GetEvacuationState()
+        {
+            if(zone)
+                return EvacuationState.Free;
+            if (zone!)
+            {
+                return EvacuationState.Evacuating;
+            }
+            return EvacuationState.Free;
+        }
     }
 }
--- DiegoTeixeiraNunes/Drones/Drones/Program.cs
+++ DiegoTeixeiraNunes/Drones/Drones/Program.cs
@@ -14,8 +14,14 @@ namespace Drones
 
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
+            List<Drone> rec = new List<Drone>();    
             Drone drone = new Drone("Joe", 100, 100);
             building bat = new building(34, 54, 100, 200, "red");
+            Rectangle r1 = new Rectangle(10, 10, 100, 150);
+            Rectangle r2 = new Rectangle(30, 100, 150, 100);
+            Rectangle r3 = new Rectangle(200, 300, 50, 50);
+            r1.IntersectsWith(r2); // true
+            r1.IntersectsWith(r3); // false
             fleet.Add(drone);
             Random random = new Random();
             try
@@ -27,7 +33,6 @@ namespace Drones
                     Drone drone1 = new Drone("Joe", x, y);
                     fleet.Add(drone1);
                 }
-
                 if (fleet.Count > 10)
                 {
                     throw new ArgumentException("salut");
@@ -37,6 +42,7 @@ namespace Drones
             {
                 Console.WriteLine(e);
             }
+           
             // Démarrage
             Application.Run(new AirSpace(fleet));
             
